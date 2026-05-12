1c88d1f4ee9a68f0f9ac068669bc6945f8dff759
Aaron Hogue <sliverninja@gmail.com>
2026-05-12 22:05:28 +0000

    Update shift-ai to 0.9.8

--- Formula/shift-ai.rb
+++ Formula/shift-ai.rb
@@ -4,9 +4,9 @@ class ShiftAi < Formula
   license "Apache-2.0"
   head "https://github.com/alohaninja/shift.git", branch: "main"
 
-  url "https://github.com/alohaninja/shift/archive/refs/tags/v0.9.7.tar.gz"
-  sha256 "2522fb7d82c9c2421061ba58c491b45273d7cc429399bbcfac4d2f3e09c90a1c"
-  version "0.9.7"
+  url "https://github.com/alohaninja/shift/archive/refs/tags/v0.9.8.tar.gz"
+  sha256 "cd7bec1ce81db235d92d85644fb428db64925a238166d74f7c7ca34af68f27fa"
+  version "0.9.8"
 
   depends_on "rust" => :build
 
